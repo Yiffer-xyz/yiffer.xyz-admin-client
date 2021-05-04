ca2a204a48572149cdc1d01ab56305d1a6270928
ragnarob <ragnarob@protonmail.com>
2021-05-04 23:11:20 +0200

    AddPage uses Select

--- src/pages/admin-panel/AddPage.vue
+++ src/pages/admin-panel/AddPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="admin-content-box" @click="openComponent" :class="{'admin-content-box-open': isOpen}">
     <h2 @click="closeComponent" class="cursorPointer">Add pages to comic</h2>
-    <span class="admin-content-box-inner description-text" v-if="isOpen">
+    <span class="admin-content-box-inner description-text" v-if="isOpen" ref="innerAdminBox">
       <p>
         - Files must be either .jpg or .png. File name does not matter, except for ordering.<br/>
         - If adding multiple pages, it is <i>very important</i> that they are named in some 
@@ -12,47 +12,66 @@
         <span class="courier">[1.jpg, 2.jpg, ...]</span> will not work for more than 9 pages!
       </p>
 
-      <ResponseMessage :message="responseMessage" :messageType="responseMessageType" @closeMessage="closeResponseMessage"
-                  class="margin-top-10"/>
+      <ResponseMessage :message="responseMessage"
+                       :messageType="responseMessageType"
+                       @closeMessage="closeResponseMessage"
+                       disableElevation
+                       class="margin-top-10"/>
 
-      <div class="horizontalFlex margin-top-8 margin-bottom-8 flex-wrap" style="align-items: center;">
-        <p style="margin-right: 8px; font-weight: 400;">Comic:</p>
-        <select v-model="comic" style="margin-bottom: 0">
-          <option v-for="comic in comicList" :key="comic.id" :value="comic">
-            {{comic.name}} 
-            {{comic.state==='finished' ? '(Finished!)' : ''}} 
-            {{comic.state === 'cancelled' ? '(Cancelled!)' : ''}}
-          </option>
-        </select>
-        <a :href="`https://yiffer.xyz/${comic.name}`"
-           v-if="comic" style="margin-left: 8px;" target="_blank" class="underline-link">
-          Go to comic <RightArrow/>
-        </a>
-      </div>
-      
-      <form enctype="multipart/form-data" novalidate>
-        <div class="pretty-input-upload">
-          <input type="file" multiple="true" @change="processFileUploadChange" id="newPageFiles" accept="image/x-png,image/jpeg" class="input-file"/>
-          <p>Select files</p>
+      <div class="verticalFlex justifyContentStart">
+        <div class="verticalFlex no-margin-bot mt-16" style="align-items: flex-end;">
+
+          <Select :options="comicOptions"
+                  title="Comic"
+                  isSearchable
+                  searchKeepSelected
+                  searchPlaceholder="Search for comic"
+                  initialWidth="16rem"
+                  :maxWidth="maxContentWidth"
+                  :searchSelected="comic ? comic.name : null"
+                  :resetValue="comicResetValue"
+                  @searchSelectedClicked="comic = null"
+                  @change="onComicSelect"/>
+
+          <a :href="`https://yiffer.xyz/${comic.name}`"
+            v-if="comic" style="margin-left: 8px;" target="_blank" class="underline-link mt-8">
+            Go to comic <RightArrow/>
+          </a>
         </div>
-      </form>
+        
+        <form enctype="multipart/form-data" novalidate class="mt-32">
+          <div class="pretty-input-upload">
+            <input type="file" multiple="true" @change="processFileUploadChange" id="newPageFiles" accept="image/x-png,image/jpeg" class="input-file"/>
+            <p>Select files</p>
+          </div>
+        </form>
 
-      <p v-if="filesAreInput" style="margin-bottom: 0px;"><b>{{selectedFiles.length}}</b> Selected files:</p>
-      <p v-if="filesAreInput" class="courier verticalFlex">
-        <span v-for="file in selectedFileNames" :key="file">{{file}}</span>
-      </p>
+        <p v-if="filesAreInput" class="mb-0 mt-8">
+          <b>{{selectedFiles.length}}</b> Selected files:
+        </p>
+        <p v-if="filesAreInput" class="courier verticalFlex">
+          <span v-for="file in selectedFileNames" :key="file">
+            {{file}}
+          </span>
+        </p>
 
-      <button @click="uploadFiles()"
-              v-if="filesAreInput && comic"
-              class="y-button margin-top-8">
-        Upload files
-        {{ comic.state === 'finished' ? ' (NOTE: this comic is marked as finished!)' : ''}}
-        {{ comic.state === 'cancelled' ? ' (NOTE: this comic is marked as cancelled!)' : ''}}
-      </button>
+        <LoadingButton @click="uploadFiles()"
+                       v-if="filesAreInput && comic"
+                       iconType="check"
+                       :isLoading="isSubmitting"
+                       class="y-button button-with-icon mt-16"
+                       :text="`
+                         Upload file${selectedFileNames.length > 1 ? 's' : ''}
+                         ${comic.state === 'finished' ? '- NOTE: Comic marked as finished!' : ''}
+                         ${comic.state === 'cancelled' ? '- NOTE: Comic marked as cancelled!' : ''}
+                       `">
+          <CheckIcon/>
+        </LoadingButton>
+      </div>
 
-      <button class="y-button y-button-neutral" @click="toggleShowAllComics()" style="margin-top:16px;"
+      <button class="y-button y-button-neutral mt-32" @click="toggleShowAllComics()"
         v-if="!showAllComics"> Show all WIP comics, with most recent page</button>
-      <button class="y-button y-button-neutral" @click="toggleShowAllComics()" style="margin-top:16px;"
+      <button class="y-button y-button-neutral mt-32" @click="toggleShowAllComics()"
         v-if="showAllComics"> Hide this list</button>
 
       <div v-if="showAllComics" class="scrolling-table-container">
@@ -98,20 +117,21 @@
 </template>
 
 <script>
-import DownArrow from 'vue-material-design-icons/ArrowDown.vue'
-import UpArrow from 'vue-material-design-icons/ArrowUp.vue'
 import RightArrow from 'vue-material-design-icons/ArrowRight.vue'
+import CheckIcon from 'vue-material-design-icons/Check.vue'
 
 import comicApi from '@/api/comicApi'
 import ResponseMessage from '@/components/ResponseMessage.vue'
+import Select from '@/components/Select.vue'
 import config from '@/config.json'
+import LoadingButton from '@/components/LoadingButton.vue'
 
 export default {
   name: 'addPage',
 
 	components: {
-    ResponseMessage,
-    DownArrow, UpArrow, RightArrow,
+    ResponseMessage, Select,
+    RightArrow, CheckIcon, LoadingButton,
 	},
 
 	props: {
@@ -126,16 +146,26 @@ export default {
       showAllComics: false,
       responseMessage: '',
       responseMessageType: 'info',
+      comicResetValue: null,
+      isSubmitting: false,
+      maxContentWidth: 9999,
     }
   },
 
   methods: {
+    onComicSelect (comicName) {
+      this.comic = this.comicList.find(c => c.name === comicName)
+      this.comicResetValue = Math.random().toString()
+    },
+
     processFileUploadChange (changeEvent) {
       this.selectedFiles = [...changeEvent.target.files]
     },
 
     async uploadFiles () {
+      this.isSubmitting = true
       let response = await comicApi.addPagesToComic(this.comic, this.selectedFiles, this.updateUploadProgress)
+      this.isSubmitting = false
 
       if (response.success) {
         this.responseMessage = 'Success updating ' + this.comic.name
@@ -181,6 +211,10 @@ export default {
   },
 
   computed: {
+    comicOptions () {
+      return this.comicList.map(c => ({text: c.name, value: c.name}))
+    },
+
     filesAreInput () { return this.selectedFiles.length > 0 },
 
     selectedFileNames () { return this.selectedFiles.map( file => file.name ) },
@@ -195,6 +229,16 @@ export default {
         })
     }
   },
+
+  watch: {
+    isOpen () {
+      setTimeout(() => {
+        if (this.$refs.innerAdminBox && document.body.clientWidth < 400) {
+          this.maxContentWidth = this.$refs.innerAdminBox.clientWidth - 32
+        }
+      }, 25)
+    }
+  }
 }
 
 let nowTimestamp = (new Date()).getTime()
